b617609155c5b7763f8b313576096d3ac27795cb
Jess Gaskell <jessfg@proton.me>
2026-03-03 14:06:38 +0100

    Refactored main menu and simplified ticket creation. Integrated optional operator assignment and removed post-create submenu.

--- helpjesk.py
+++ helpjesk.py
@@ -2,23 +2,34 @@ tickets = []
 operators = ["Lara", "Mario", "Luigi"]
 next_ticket_id = 1
 
-def print_error(error):
-    # Skriver ut felmeddelanden i rött (ANSI escape codes).
+def print_red(message):
 
     print("")
-    print(f"\033[31m{error}\033[0m")
+    print(f"\033[31m{message}\033[0m")
     print("")
 
-def ask_until_valid(get_function, message, print_error_message): # generic loop
+def ask_until_valid(get_function, message, error_message):
+    
     while True:
         result = get_function(message)
 
         if result is not None:
             return result
         else:
-            print_error(print_error_message)
+            print_red(error_message)
 
-def get_priority(prio_message):
+def main_menu_choice(mm_message):
+
+    mm_options= ["1", "2", "3", "0"]    
+
+    mm_choice = (input(mm_message)).strip()
+
+    if mm_choice in mm_options:
+        return int(mm_choice)
+    else:
+        return None
+
+def get_priority_choice(prio_message):
 
     prio_choice = input(prio_message).strip()
     
@@ -38,7 +49,6 @@ def find_ticket_by_id(tickets, ticket_id):
             return ticket
     return None
 
-
 def create_ticket():
     global next_ticket_id
     print("")
@@ -49,7 +59,30 @@ def create_ticket():
     
     description = input("Describe problem: ") # More detailed description
     
-    priority = ask_until_valid(get_priority, "Choose priority level: 1.Low, 2.Medium, 3.High: ", "Error, invalid choice. Valid options: 1, 2 or 3.")
+    priority = ask_until_valid(get_priority_choice, "Choose priority level: 1.Low, 2.Medium, 3.High: ", "Error, invalid choice. Valid options: 1, 2 or 3.")
+
+    while True:    
+        for index, name in enumerate(operators, start=1):
+            print(f"{index}. {name}")
+        
+        operator_choice = input("Select an operator or press Enter to cancel: ").strip()
+        
+        if operator_choice == (""):
+            operator_choice = "Unassigned"
+            break
+        if not operator_choice.isdigit():
+            print_red("Error: Invalid operator.")
+            continue
+
+        operator_choice = int(operator_choice)
+
+        
+        if operator_choice < 1 or operator_choice > len(operators):
+            print_red("Error: Invalid operator.")
+            continue
+
+        operator_choice = operators[operator_choice - 1]
+        break
 
     ticket = { # Ticket disctionary
         "id": next_ticket_id,
@@ -57,7 +90,7 @@ def create_ticket():
         "description": description,
         "status": "New",
         "priority": priority,
-        "assigned_to": "Unassigned",
+        "assigned_to": operator_choice,
         "notes": []}
     
     tickets.append(ticket)
@@ -71,59 +104,8 @@ def create_ticket():
     print("Description:", description)
     print("Priority:", priority)
     print("Assigned to:", ticket["assigned_to"])
-    
-    # --- Post-create options menu ---
-
-    while True:  
-        print("-------------------------------")
-        print("   TICKET OPTIONS")
-        print("-------------------------------")
-        print("1. Assign operator | 2. Add note | Enter to return to main menu")
-
-        new_ticket_option = input("Choose an option: ").strip()
-
-        if new_ticket_option == (""):
-            return
-        if not new_ticket_option.isdigit():
-            print("Error: Select 1 or 2, or press Enter to cancel: ")
-            continue
-        
-
-    # --- Option 1: Assign operator ---
-
-        if new_ticket_option == ("1"):
-
-            print("-------------------------------")
-            print("   ASSIGN OPERATOR")
-            print("-------------------------------")  
-            while True:    
-                for index, name in enumerate(operators, start=1):
-                    print(f"{index}. {name}")
-                
-                op_choice = input("Select an operator or press Enter to cancel: ").strip()
-                
-                if op_choice == (""):
-                    break
-                if not op_choice.isdigit():
-                    print("Error: Invalid operator.")
-                    continue
-                op_choice = int(op_choice)
-                if op_choice < 1 or op_choice > len(operators):
-                    print("Error: Invalid operator.")
-                    continue
-                
-                ticket["assigned_to"] = operators[op_choice - 1]
-
-                print("Assigned operator:", ticket["assigned_to"])
-                break
-        
-    # --- Option 2: Add note ---
-
-        elif new_ticket_option == ("2"):
-            print("add note under construction")
+    return
                
-
-
 def view_tickets():  
     if tickets == []:
         print("No tickets found.")
@@ -188,14 +170,14 @@ def view_tickets():
 
                 print("1. Edit | 2. Back to ticket list | Enter to return to main menu")
                 
-                det_sub_choice = input("Choose an option: ").strip()
+                details_sub_choice = input("Choose an option: ").strip()
                 
-                if det_sub_choice == "":
+                if details_sub_choice == "":
                     return
-                if det_sub_choice == "1":
+                if details_sub_choice == "1":
                     print("Edit function under construction")
                     continue #temporary
-                elif det_sub_choice == "2":
+                elif details_sub_choice == "2":
                     choose_another = True
                     break
                 else:
@@ -214,34 +196,35 @@ def view_tickets():
                     print(f"Ticket ID: {ticket['id']} | Title: {ticket['title']} | Status: {ticket['status']} | Priority: {ticket['priority']} | Assigned to: {ticket['assigned_to']}")
             continue
 
-
 def edit_ticket():
     print("Ticket Editing under construction")
     input("Press Enter to return to main menu")
 
+def main():
+    print("Welcome to HELPJESK version 0.1")
+    while True:
+        print("")
+        print("-------------------------------")
+        print("   MAIN MENU")
+        print("-------------------------------")
 
+        print("1. Create ticket")
+        print("2. View all tickets")
+        print("3. Edit ticket")
+        print("0. Exit ticketing system")
 
-print("Welcome to HELPJESK version 0.1")
-while True:
-    print("")
-    print("-------------------------------")
-    print("   MAIN MENU")
-    print("-------------------------------")
+        mm_choice = main_menu_choice("Choose an option: ")
 
-    print("1. Create ticket")
-    print("2. View all tickets")
-    print("3. Edit ticket")
-    print("0. Exit ticketing system")
+        if mm_choice == 1:
+            create_ticket()
+        elif mm_choice == 2:
+            view_tickets()
+        elif mm_choice == 3:
+            edit_ticket()
+        elif mm_choice == 0:
+            break
+        else:
+            print_red("Error: invalid choice, try again")
+            continue
 
-    mm_choice = input("Choose an option: ")
-    if mm_choice == "1":
-        create_ticket()
-    elif mm_choice == "2":
-        view_tickets()
-    elif mm_choice == "3":
-        edit_ticket()
-    elif mm_choice == "0":
-        break
-    else:
-        print("Error: invalid choice, try again")
-        continue
+main()
